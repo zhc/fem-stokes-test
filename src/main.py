6e4aefde3d9416b17a8382edce56fdef44382a7e
vitek <refactorman@gmail.com>
2012-10-31 16:06:58 +1000

    removed square example

--- src/main.py
+++ src/main.py
@@ -1,40 +1,135 @@
 from plotter import *
 from solver import *
 
-ff = ("28*pi*pi*sin(4*pi*x[0])*cos(4*pi*x[1])", "-36*pi*pi*cos(4*pi*x[0])*sin(4*pi*x[1])")
-uu = ("sin(4*pi*x[0])*cos(4*pi*x[1])", "-cos(4*pi*x[0])*sin(4*pi*x[1])")
-pp = ("pi*cos(4*pi*x[0])*cos(4*pi*x[1])")
 elements_table = {
     "CR": ("Crouzeix-Raviart", 1, "Discontinuous Lagrange", 0),    
     "CD": ("Lagrange", 2, "Discontinuous Lagrange", 0),    
-    "TH": ("Lagrange", 2, "Lagrange", 1)
+    "TH": ("Lagrange", 2, "Lagrange", 1),
+    
+#    "TH1": ("Lagrange", 1, "Lagrange", 1), # doesnt work
+#    "TH2": ("Lagrange", 2, "Lagrange", 2), # strange solution, form like eight
+#    "TH3": ("Lagrange", 3, "Lagrange", 2), # like a normal solution 
+#    "TH4": ("Lagrange", 3, "Lagrange", 3), # like a normal solution, not sure
+#    "TH5": ("Lagrange", 3, "Lagrange", 1), # like a normal solution, not sure
+#    
+#    "CR1": ("Crouzeix-Raviart", 2, "Discontinuous Lagrange", 1), # invalid degree
+#    
+#    "CD1": ("Lagrange", 3, "Discontinuous Lagrange", 1), # like a normal solution
+#    "CD2": ("Lagrange", 1, "Discontinuous Lagrange", 0), # doesnt work    
+#    "CD3": ("Lagrange", 1, "Discontinuous Lagrange", 1), # doesnt work     
+#    "CD4": ("Lagrange", 3, "Discontinuous Lagrange", 0), # like a normal solution, not sure     
     }
-h = 20
-hmax = 100
-hstep = 20
+hmin = 20
+hmax = 150
+hstep = 10
+h_ex = 200
+
+times = {}
+u_errs = {}
+p_errs = {}
+psi_errs = {}
+ 
+def save_pvd(name, data):
+    f = File(name+".pvd")
+    f << data    
+    
+def save_pvd_list(u, p, psi, name, h):    
+    save_pvd("result_velocity_%s_%d" % (name, h), u)
+    save_pvd("result_pressure_%s_%d" % (name, h), p)
+    save_pvd("result_psi_%s_%d" % (name, h), psi)
+
+def save_time(name, h, time):    
+    if h not in times:
+        times[h] = {}
+    times[h][name] = time
+    
+def save_u_err(name, h, u_err):    
+    if h not in u_errs:
+        u_errs[h] = {}
+    u_errs[h][name] = u_err
+
+def save_p_err(name, h, p_err):    
+    if h not in p_errs:
+        p_errs[h] = {}
+    p_errs[h][name] = p_err
+    
+def save_psi_err(name, h, psi_err):    
+    if h not in psi_errs:
+        psi_errs[h] = {}
+    psi_errs[h][name] = psi_err
+    
+
+def save_dat_aggr(fname, times):    
+    ft = open("result_%s.dat" % fname, "w")
+    
+    arr_h = range(hmin, hmax+1, hstep)
+    arr_n = []
+    for name in times[hmin]:
+        arr_n.append(name)
+        
+    ft.write("# h ")
+    for name in arr_n:
+        ft.write("%s " % name)
+    ft.write("\n")
+    for h in arr_h:
+        ft.write("%.9f " % h)
+        for name in arr_n:
+            ft.write("%.9f " % (times[h][name]))
+        ft.write("\n")
+    ft.close()
+    
+def save_result():    
+    save_dat_aggr("time", times)
+    save_dat_aggr("u_err", u_errs)
+    save_dat_aggr("psi_err", psi_errs)
+    save_dat_aggr("p_err", p_errs)
+    
+def save_dat(u, p, psi, name, h, mesh):
+    coords = mesh.coordinates()    
+    fu = open("result_velocity_%s_%d.dat" % (name, h), "w")
+    fp = open("result_pressure_%s_%d.dat" % (name, h), "w")
+    fps = open("result_psi_%s_%d.dat" % (name, h), "w")
+    fu.write("# x y ux uy\n")
+    fp.write("# x y p\n")
+    fps.write("# x y psi\n")
+    last_y = coords[0][1]    
+    for i in coords:
+        yu = u(i)
+        yp = p(i)
+        yps = psi(i)
+        if last_y != i[1]:            
+            fu.write("\n")
+            fp.write("\n")
+            fps.write("\n")
+        fu.write("%.9f %.9f %.9f %.9f\n" % (i[0], i[1], yu[0], yu[1] ))
+        fp.write("%.9f %.9f %.9f\n" % (i[0], i[1], yp ))
+        fps.write("%.9f %.9f %.9f\n" % (i[0], i[1], yps ))
+        last_y = i[1]        
+    fu.close()
+    fp.close()
+    fps.close()
 
 if __name__ == "__main__":
     init_result()
-    mesh_ex = create_mesh(150)
+    mesh_ex = create_mesh(h_ex)
     space_ex = create_space("TH", mesh_ex)
-    (u_ex, p_ex, ttt) = solve_cavity(mesh_ex, space_ex)
+    (u_ex, p_ex, t_ex) = solve_cavity(mesh_ex, space_ex)
+    (psi_ex) = solve_cavity_psi(mesh_ex, space_ex, u_ex)
+    save_pvd_list(u_ex, p_ex, psi_ex, "TH", h_ex)
+    save_dat(u_ex, p_ex, psi_ex, "TH", h_ex, mesh_ex)
+    h = hmin
     while(h<=hmax):
         mesh = create_mesh(h)            
         for element in elements_table:        
-            space = create_space(element, mesh)
-#            (u, p, time) = solve_square(mesh, space)
-#            collect_square_result(element, h, u, p, time, mesh)
-            
-#            (u_err, p_err) = compare_square(u, p, u_ex, p_ex, mesh)
-#            collect_square_error(element, h, u_err, p_err)
-            
+            space = create_space(element, mesh)            
             (u, p, time) = solve_cavity(mesh, space)
             (psi) = solve_cavity_psi(mesh, space, u)
-            
-            collect_cavity_result(element, h, u, p, psi, time, mesh)
-            (u_err, p_err) = compare_square(u, p, u_ex, p_ex, mesh)
-            collect_square_error(element, h, u_err, p_err)
-            
-            
+            (u_err, p_err, psi_err) = solve_cavity_error(u, p, psi, u_ex, p_ex, psi_ex, mesh_ex)
+            save_pvd_list(u, p, psi, element, h)
+            save_time(element, h, time)
+            save_u_err(element, h, u_err)
+            save_p_err(element, h, p_err)
+            save_psi_err(element, h, psi_err)
+            save_dat(u, p, psi, element, h, mesh_ex) 
         h+=hstep
     save_result()
--- src/solver.py
+++ src/solver.py
@@ -14,55 +14,6 @@ def create_space(element, mesh):
     W = U*P
     return W
     
-def solve_square(mesh, space):
-    print "solve square"
-    def domain_all(x, on_boundary):
-        return on_boundary
-    bc0 = DirichletBC(space.sub(0), Expression(main.uu), domain_all)
-    bcs = [bc0]
-    
-    (u, p) = TrialFunctions(space)
-    (v, q) = TestFunctions(space)
-    f = Expression(main.ff)       
-    a = inner(grad(u), grad(v))*dx - p*div(v)*dx + div(u)*q*dx
-    L = inner(f, v)*dx
-    
-    t = Timer("Solve timing");
-    t.start()
-            
-    U = Function(space)
-    problem = LinearVariationalProblem(a, L, U, bcs=bcs)
-    solver = LinearVariationalSolver(problem)
-    solver.solve()
-    
-    t.stop()
-    
-    (u, p) = U.split(True)
-    
-    # correct pressure field sum equals 0
-    c = assemble(p*dx)
-    vec = p.vector()
-    for i in range(vec.size()):
-        vec[i] -= c
-                
-    return u, p, t.value()
-    
-def solve_square_ex(mesh):
-    print "solve square exact"
-    VP10 = VectorFunctionSpace(mesh, "CG", 10)
-    P10 = FunctionSpace(mesh, "CG", 10)
-    u_ex = Expression(main.uu, element=VP10.ufl_element())
-    p_ex = Expression(main.pp, element=P10.ufl_element())
-    return u_ex, p_ex
-
-def compare_square(u, p, u_ex, p_ex, mesh):
-    print "compare square"
-    M = inner((u_ex - u),(u_ex - u))*dx
-    v_err = assemble(M, mesh=mesh)    
-    Mp = (p_ex - p)*(p_ex - p)*dx
-    p_err = assemble(Mp, mesh=mesh)
-    return v_err, p_err
-    
 def solve_cavity(mesh, space):
     print "solve cavity"
     def domain_top(x, on_boundary):
@@ -85,6 +36,14 @@ def solve_cavity(mesh, space):
     solver.solve()    
     t.stop()    
     (u, p) = U.split(True)    
+    
+    # correct pressure
+    c = assemble(p*dx)
+    vec = p.vector()
+    for i in range(vec.size()):
+        vec[i] -= c
+                
+    
     return u, p, t.value()
 
 
@@ -106,3 +65,12 @@ def solve_cavity_psi(mesh, space, u):
     solver = LinearVariationalSolver(problem)
     solver.solve()
     return PSI
+
+def solve_cavity_error(u, p, psi, u_ex, p_ex, psi_ex, mesh_ex):
+    M = inner((u_ex - u),(u_ex - u))*dx
+    v_err = assemble(M, mesh=mesh_ex)    
+    Mp = (p_ex - p)*(p_ex - p)*dx
+    p_err = assemble(Mp, mesh=mesh_ex)
+    Mps = (psi_ex - psi)*(psi_ex - psi)*dx
+    psi_err = assemble(Mps, mesh=mesh_ex)
+    return (v_err, p_err, psi_err)
